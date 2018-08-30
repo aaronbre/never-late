66cfcd72bf2a9289256bc00268ddfe07ecab2048
aaronbrecher <aribrecherdev@gmail.com>
2018-08-30 15:46:15 -0400

    ready to merge

--- app/src/main/java/com/aaronbrecher/neverlate/ui/activities/MainActivity.java
+++ app/src/main/java/com/aaronbrecher/neverlate/ui/activities/MainActivity.java
@@ -5,21 +5,26 @@ import android.app.NotificationManager;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.aaronbrecher.neverlate.BuildConfig;
 import com.aaronbrecher.neverlate.Constants;
@@ -165,20 +170,39 @@ public class MainActivity extends AppCompatActivity implements ListItemClickList
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        //TODO maybe only run this if there is network...
         if (item.getItemId() == R.id.main_activity_menu_sync) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    mViewModel.deleteAllEvents();
-                    mEventList = CalendarUtils.getCalendarEventsForToday(MainActivity.this);
-                    mViewModel.insertEvents(mEventList);
-                    BackgroundUtils.getLocation(MainActivity.this, MainActivity.this, mLocationProviderClient);
-                }
-            }).start();
-
+            if (SystemUtils.isConnected(this)) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mViewModel.deleteAllEvents();
+                        mEventList = CalendarUtils.getCalendarEventsForToday(MainActivity.this);
+                        mViewModel.insertEvents(mEventList);
+                        BackgroundUtils.getLocation(MainActivity.this, MainActivity.this, mLocationProviderClient);
+                    }
+                }).start();
+            } else {
+                showNoConnectionSnackbar();
+            }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showNoConnectionSnackbar() {
+        Snackbar.make(mListContainer, R.string.refresh_error_no_connection, Snackbar.LENGTH_INDEFINITE)
+                .setAction(getString(R.string.launch_data_settings_button), new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(Intent.ACTION_MAIN);
+                        intent.setComponent(
+                                new ComponentName(
+                                        "com.android.settings",
+                                        "com.android.settings.Settings$DataUsageSummaryActivity"
+                                ));
+                        startActivity(intent);
+                    }
+                })
+                .show();
     }
 
     @Override
@@ -197,8 +221,7 @@ public class MainActivity extends AppCompatActivity implements ListItemClickList
         new Thread(new Runnable() {
             @Override
             public void run() {
-                if(SystemUtils.isConnected(MainActivity.this))
-                    DirectionsUtils.addDistanceInfoToEventList(mGeoApiContext, mEventList, location);
+                DirectionsUtils.addDistanceInfoToEventList(mGeoApiContext, mEventList, location);
                 mViewModel.insertEvents(mEventList);
                 Geofencing geofencing = Geofencing.builder(mEventList);
                 geofencing.createAndSaveGeofences();
